1d1b763f69936ecd13c4bd5ef02da463e6fea913
oussamapromax <oussamafarhani327@gmail.com>
2025-02-18 17:45:06 +0100

    correction de reservationrouter

--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const reservationController = require('../controllers/reservationController');
 
-router.post('/', reservationController.createReservation);
-router.get('/', reservationController.getReservations);
-router.get('/:id', reservationController.getReservationById);
-router.put('/:id', reservationController.updateReservation);
-router.delete('/:id', reservationController.deleteReservation);
+router.post('/', reservationController.createReservation); // Créer une réservation
+router.get('/', reservationController.getReservations); // Obtenir toutes les réservations
+router.get('/:id', reservationController.getReservationById); // Obtenir une réservation spécifique
+router.put('/:id', reservationController.updateReservation); // Modifier une réservation
+router.delete('/:id', reservationController.deleteReservation); // Supprimer une réservation
+
+// Routes supplémentaires pour gérer le statut des réservations
+router.put('/:id/annuler', reservationController.annulerReservation); // Annuler une réservation
+router.put('/:id/confirmer', reservationController.confirmerReservation); // Confirmer une réservation
 
 module.exports = router;
